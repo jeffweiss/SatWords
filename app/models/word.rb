4bbb4249b80d6c3a1200d22aa1a667dc87dffe37
Jeff Weiss <jeff.a.weiss@gmail.com>
2011-07-02 23:11:34 -0700

    made words sortable

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,4 +4,8 @@ class Word < ActiveRecord::Base
   validates :word, :presence   => true,
                    :length     => { :maximum => 50 },
                    :uniqueness => { :case_sensitive => false }
+
+  def <=>(word)
+    self.word <=> word.word
+  end
 end
