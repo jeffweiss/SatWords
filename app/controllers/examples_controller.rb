2b5dbe0ad90e2967f56f6a8e1ab810497a5023d8
Jeff Weiss <jeff.a.weiss@gmail.com>
2011-07-05 22:41:25 -0700

    create example tests passing

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -2,6 +2,16 @@ class ExamplesController < ApplicationController
   before_filter :authenticate
 
   def create
+    @word = params[:word]
+    @examples = @word.examples.clone
+    @example = @word.examples.build(params[:example])
+    if @example.save
+      flash[:success] = "Example created!"
+      redirect_to word_path(@word)
+    else
+      @definitions = @word.definitions
+      render 'words/show'
+    end
   end
 
   def destroy
--- spec/controllers/examples_controller_spec.rb
+++ spec/controllers/examples_controller_spec.rb
@@ -14,4 +14,50 @@ describe ExamplesController do
       response.should redirect_to(signin_path)
     end
   end
+  
+  describe "POST 'create'" do
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+      @word = Factory(:word)
+    end
+
+    describe "failure" do
+      before(:each) do
+        @attr = { :content => "" }
+      end
+
+      it "should not create an example" do
+        lambda do
+          post :create, :example => @attr, :word => @word
+        end.should_not change(Definition, :count)
+      end
+
+      it "should render the word show page" do
+        post :create, :example => @attr, :word => @word
+        response.should render_template('words/show')
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = { :content => "Lorem ipsum" }
+      end
+
+      it "should create an example" do
+        lambda do
+          post :create, :example => @attr, :word => @word
+        end.should change(Example, :count).by(1)
+      end
+
+      it "should redirect to the word page" do
+        post :create, :example => @attr, :word => @word
+        response.should redirect_to(word_path(@word))
+      end
+
+      it "should have a flash message" do
+        post :create, :example => @attr, :word => @word
+        flash[:success].should =~ /example created/i
+      end
+    end
+  end
 end
